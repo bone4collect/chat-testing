48ddd317339098c6ea356f2b19e5f2ec7d2e2eeb
bone4collect <amine.belhadjmassoud@esen.tn>
2023-03-02 00:52:24 +0100

    first

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+
+import $ from "jquery";
+import Gun from "gun";
+
+export default function Home() {
+  useEffect(() => {
+    const gun = Gun().get("thoughts");
+    $("form").on("submit", function (event) {
+      event.preventDefault();
+      gun.set($("input").val());
+      $("input").val("");
+    });
+    gun.map().on(function (thought, id) {
+      const li = $("#" + id).get(0) || $("<li>").attr("id", id).appendTo("#todo-list");
+      if (thought) {
+        $(li).text(thought);
+      } else {
+        $(li).hide();
+      }
+    });
+    $("body").on("dblclick", "li", function (event) {
+      gun.get(this.id).put(null);
+    });
+  }, []);
+
+  return (
+    <div>
+      
+      <h1>To Do App</h1>
+      <form>
+        <input /> <button>Thoughts</button>
+      </form>
+      <ul id="todo-list"></ul>
+    </div>
+  );
+}
